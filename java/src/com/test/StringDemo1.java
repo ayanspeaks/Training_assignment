cb4a1dedbf531afa4f9d201817391bfe772d9ff8
ayanspeaks <ayans.2003kali@gmail.com>
2025-02-28 15:30:49 +0530

    string

--- java/src/com/test/StringDemo1.java
+++ java/src/com/test/StringDemo1.java
@@ -1,5 +1,25 @@
 package com.test;
 
 public class StringDemo1 {
+	
+	public static void main(String a[]) {
+		String str1 = "java";
+		String str2 = "java";
+		
+		String str3 = new String("java");
+		String str4 = new String("java");
+		
+		String str5 = "hello";
+		
+		System.out.println(str1 == str2);
+		System.out.println(str3 == str4);
+		System.out.println(str1 == str3);
+		System.out.println(str2 == str5);
+		System.out.println(str1.equals(str3));
+		
+		String str7 = str1.concat(" ayan");//immutable
+		
+		System.out.println(str7);
+	}
 
 }
